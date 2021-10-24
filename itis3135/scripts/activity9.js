bc789a70abffa43a2cabb2cc6f39e9400ded0e03
kshirani <kshirani@uncc.edu>
2021-10-23 20:15:42 -0400

    Activity 9 - Slideshow added

---
+++ itis3135/scripts/activity9.js
@@ -0,0 +1,27 @@
+//Help source: page 255 of text book "murach's JavaScript and jQuery 3rd edition"
+$(document).ready(function() {
+// preload images
+$("#image_list a").each (function() {
+    var swappedImage = new Image();
+    swappedImage.src = $(this).attr("href");
+});
+
+// set up event handler for links
+$("#image_list a").click (function(evt) {
+    // swap image
+    var imageURL = $(this).attr("href");
+    $("#main_image").attr("src", imageURL);
+
+    //swap caption
+    var caption = $(this).attr("title");
+    $("#caption").text(caption);
+
+    //cancel the default action of the link
+    evt.preventDefault(); //jQuery cross browser method
+}); //end click
+
+// move focus to first thumbnail
+    $("li:first-child a").focus();
+}); //end ready
+
+
