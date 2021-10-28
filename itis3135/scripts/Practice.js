251befb05b1493b97644911256706e879f019c89
kshirani <kshirani@uncc.edu>
2021-10-27 22:43:12 -0400

    Added the midterm practice activity.

--- itis3135/scripts/Practice.js
+++ itis3135/scripts/Practice.js
@@ -1,20 +1,32 @@
-var $ = function(id) {
-	return document.getElementById(id);
-};
 
-window.onload = function() {
+let total = 0;
+function espressoFunc(){
+	document.getElementById("order").innerHTML += "$1.95 - Espresso - Delicious Espresso. Wanna try it? <br>";
+	total += 1.95;
+	document.getElementById("total").innerHTML = "$" + total.toFixed(2);
+}
 
+function cappuccinoFunc(){
+	document.getElementById("order").innerHTML += "$3.45 - Cappuccino - Delicious Cappuccino! <br>";
+	total += 3.45;
+	document.getElementById("total").innerHTML = "$" + total.toFixed(2);
+}
 
-	//add onclick event handler for each image
-	var a= document.getElementById(espresso);
-	a[0].onclick= order;
-	// for click event add item to order and update total
-	var order =function () {
-
-		document.getElementById(order).innerHTML += "Delicious espresso. Wanna try it?";
+function placeOrder(){
+	if (document.getElementById("order").innerHTML === ""){
+		alert("You didn't say what you want!!!")
+	}else{
+		document.getElementById("result").innerHTML = "Your Order Was Placed! Order Number:" + Math.floor(Math.random() * (999 - 1 + 1) + 1);
 	}
-	// display order and total
-
 	
-		
-}; // end onload
+}
+
+function clearOrder(){
+	document.getElementById("order").innerHTML = "";
+	document.getElementById("total").innerHTML = "";
+	document.getElementById("result").innerHTML = "";
+}
+
+function contact(){
+	alert("You can call: 888-222-333 or email: tuxedo_cat@gmail.com to contact us! ");
+}
