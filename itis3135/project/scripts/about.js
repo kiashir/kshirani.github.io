2291b80326e9de0663fdf257e3abbf700dda0735
kshirani <kshirani@uncc.edu>
2021-12-02 13:26:11 -0500

    Made some fixes for the "about" page of the projecte

--- itis3135/project/scripts/about.js
+++ itis3135/project/scripts/about.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
     $.ajax({
         type: "get",
         url: "about-project.json",
@@ -16,13 +16,42 @@ $(document).ready(function() {
             $.each(data, function(){
                 $.each(this, function(key, value){
                   $("#about").append(
-                     "<h3>" + value.title + "</h3>" +
-                     value.owner + "<br>" + 
-                     value.purpose + "<br><br>"
+                     "Project Title: " + value.title + "<br>" +
+                     "Project Owner (client): " + value.owner + "<br>" + 
+                     "Project Purpose: "+ value.purpose + "<br><br>"
                   )
               })  
             })
 
         }
     });
-});
+});*/
+
+function projectInfo(){
+    $.ajax({
+        type: "get",
+        url: "about-project.json",
+        beforeSend: function() {
+            $("#about").html("Loading...");
+        },
+        timeout: 10000,
+        error: function(xhr, status, error) {
+            alert("Error: " + xhr.status + " - " + error);
+        },
+        dataType: "json",
+        success: function(data) {
+            $("#about").html("");
+//Reference: murach's JavaScript and jQuery 3rd edition - page 351
+            $.each(data, function(){
+                $.each(this, function(key, value){
+                  $("#about").append(
+                     "<span style='color:yellow; font-size:larger;'> Project Title: </span>" + value.title + "<br>" +
+                     "<span style='color:yellow; font-size:larger;'> Project Owner (client): </span>" + value.owner + "<br>" + 
+                     "<span style='color:yellow; font-size:larger;'> Project Purpose: </span>"+ value.purpose + "<br><br>"
+                  )
+              })  
+            })
+
+        }
+    });
+}
